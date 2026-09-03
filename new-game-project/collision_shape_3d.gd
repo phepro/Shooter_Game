640142e73e37a7cb957b2a37d7d02448d9d5b3dd
crocodile_lover <158551266+phepro@users.noreply.github.com>
2026-09-03 19:15:45 +0200

    wfewef

---
+++ new-game-project/collision_shape_3d.gd
@@ -0,0 +1,9 @@
+extends CollisionShape3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	CollisionShape3D.shape.set_faces(MeshInstance3D.)
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
